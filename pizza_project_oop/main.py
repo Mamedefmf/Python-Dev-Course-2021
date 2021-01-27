afd5ebdd80e845a9b7a696747efc628ff4787a56
mamede-fmf <63212634+mamedefmf@users.noreply.github.com>
2021-01-27 14:06:07 -0300

    Test commit
    Signed-off-by: mamede-fmf <63212634+Mamedefmf@users.noreply.github.com>

--- pizza_project_oop/main.py
+++ pizza_project_oop/main.py
@@ -1,18 +1,19 @@
 # Creates the pizza class with the following parameters: name, price, ingredients, vegetarian(t or f)
 
 class Pizza:
-    def __init__(self):
-        self.name() = ""
-        self.price() = .0
+    def __init__(self, name, price, ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
 
     def display(self):
         print(f"{self.name} : R${self.price}")
+        print(", ".join(self.ingredients))
+
 
 # Creates object: pizza1
-pizza1 = Pizza()
-pizza1.name = "portuguesa"
-pizza.price = 24.99
-
+pizza1 = Pizza("portuguesa", 24.99, ("muzzarela cheese", "ham", "egg", "onion", "tomato"))
+pizza1.display()
 
 # a list with pizza flavors from the last project
-#pizza_container = ["portuguesa", "paulista", "baiana", "frango e catupity", "atum"]
+# pizza_container = ["portuguesa", "paulista", "baiana", "frango e catupity", "atum"]
