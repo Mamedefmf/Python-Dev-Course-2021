e3f2163fc94b90577d915cc797df37f38b877c7a
mamede-fmf <63212634+Mamedefmf@users.noreply.github.com>
2021-01-27 18:05:46 -0300

    v0.0.3
    Signed-off-by: mamede-fmf <63212634+Mamedefmf@users.noreply.github.com>

--- pizza_project_oop/main.py
+++ pizza_project_oop/main.py
@@ -17,27 +17,45 @@ class Pizza:
 
 
 # Creates a child class for a custom pizza, where the user can choose the ingredients and the price will be attached
-# class CustomPizza(Pizza):
+class CustomPizza(Pizza):
+    BASE_PRICE = 20.0
+    PRICE_PER_INGREDIENT = 3.2
+
+    def __init__(self):
+        super().__init__("Custom", .0, [])
+        self.ask_user_for_ingredients()
+        self.compute_price()
+
+    def ask_user_for_ingredients(self):
+        while True:
+            ingredient = input("Add an Ingredient (or press ENTER to finish) ")
+            if ingredient == "":
+                return
+            self.ingredients.append(ingredient)
+            print(f"You have added {len(self.ingredients)} ingredient(s) : {', '.join(self.ingredients)}")
+
+    def compute_price(self):
+        self.price = float(len(self.ingredients)) * self.PRICE_PER_INGREDIENT + self.BASE_PRICE
 
 #
 pizzas = [
-    Pizza("portuguesa", 8.99, ("muzzarela cheese", "ham", "egg", "onion", "tomato")),
-    Pizza("paulista", 10.99, ("muzzarela cheese", "bacon", "tomato")),
-    Pizza("baiana", 11, ("muzzarela cheese", "pepperoni sausage", "pepper sauce", "tomato")),
-    Pizza("chicken & cheese", 10, ("chicken", "catupiry cheese")),
-    Pizza("tuna", 12.99, ("muzzarela cheese", "tuna", "tomato")),
-    Pizza("broccoli", 10, ("broccoli", "catupiry cheese", "tomato"), True),
-    Pizza("special broccoli", 16.99, ("brocolli", "muzzarela cheese", "catupiry cheese", "tomato"), True)
+    Pizza("portuguesa", 29.99, ("muzzarela cheese", "ham", "egg", "onion", "tomato")),
+    Pizza("paulista", 29.99, ("muzzarela cheese", "bacon", "tomato")),
+    Pizza("baiana", 26.99, ("muzzarela cheese", "pepperoni sausage", "pepper sauce", "tomato")),
+    Pizza("chicken & cheese", 24.99, ("chicken", "catupiry cheese")),
+    Pizza("tuna", 24.99, ("muzzarela cheese", "tuna", "tomato")),
+    Pizza("broccoli", 24.99, ("broccoli", "catupiry cheese", "tomato"), True),
+    Pizza("special broccoli", 29.99, ("brocolli", "muzzarela cheese", "catupiry cheese", "tomato"), True),
+    CustomPizza()
 ]
 
-
 # Function to sort the pizza list by the number of ingredients
-def pizza_sort(e):
-    return len(e.ingredients)
+# def pizza_sort(e):
+#    return len(e.ingredients)
 
 
 # Call the pizza_sort function to organize the pizzas list
-pizzas.sort(key=pizza_sort)
+# pizzas.sort(key=pizza_sort)
 
 
 for i in pizzas:
