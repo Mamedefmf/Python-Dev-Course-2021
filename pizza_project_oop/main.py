b88ed8e4f2fb1a6cb68c7267a5373fb445865ef9
mamede-fmf <63212634+Mamedefmf@users.noreply.github.com>
2021-01-27 13:47:00 -0300

    pizza_project_oop_v0.0.1
    Creates the Pizza class

---
+++ pizza_project_oop/main.py
@@ -0,0 +1,18 @@
+# Creates the pizza class with the following parameters: name, price, ingredients, vegetarian(t or f)
+
+class Pizza:
+    def __init__(self):
+        self.name() = ""
+        self.price() = .0
+
+    def display(self):
+        print(f"{self.name} : R${self.price}")
+
+# Creates object: pizza1
+pizza1 = Pizza()
+pizza1.name = "portuguesa"
+pizza.price = 24.99
+
+
+# a list with pizza flavors from the last project
+#pizza_container = ["portuguesa", "paulista", "baiana", "frango e catupity", "atum"]
