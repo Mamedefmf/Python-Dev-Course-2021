af950671d0b81ffdb726b654cce67eb30258959d
Mamede <63212634+mamedefmf@users.noreply.github.com>
2021-01-27 14:28:19 -0300

    1. Creates pizza1,2,3,4,5. 2. Group all pizzas in one tuple (pizzas) 3. Display all pizzas
    Signed-off-by: mamede-fmf <63212634+Mamedefmf@users.noreply.github.com>

--- pizza_project_oop/main.py
+++ pizza_project_oop/main.py
@@ -11,9 +11,17 @@ class Pizza:
         print(", ".join(self.ingredients))
 
 
-# Creates object: pizza1
-pizza1 = Pizza("portuguesa", 24.99, ("muzzarela cheese", "ham", "egg", "onion", "tomato"))
-pizza1.display()
+# Objective: 1.create a pizzas tuple 2.create the pizzas from the pizza_container 3.display all pizzas
 
-# a list with pizza flavors from the last project
-# pizza_container = ["portuguesa", "paulista", "baiana", "frango e catupity", "atum"]
+# Creates objects: pizza1,pizza2,3....
+pizza1 = Pizza("portuguesa", 24.99, ("muzzarela cheese", "ham", "egg", "onion", "tomato"))
+pizza2 = Pizza("paulista", 34.99, ("muzzarela cheese", "bacon", "tomato"))
+pizza3 = Pizza("baiana", 26.99, ("muzzarela cheese", "pepperoni sausage", "pepper sauce", "tomato"))
+pizza4 = Pizza("chicken & cheese", 24.99, ("chicken", "catupiry cheese"))
+pizza5 = Pizza("tuna", 24.99, ("muzzarela cheese", "tuna", "tomato"))
+
+pizzas = (pizza1, pizza2, pizza3, pizza4, pizza5)
+
+for i in pizzas:
+    print("")
+    i.display()
